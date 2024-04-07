32d3ab38562618a6ce692e8810441798542ce9b6
matteodelseppia <matteodelseppia.cc@gmail.com>
2024-04-07 15:46:58 +0200

    .

---
+++ build/src/CMakeFiles/example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/matteodelseppia/Desktop/cpp_cmake_catch2_project_template/bin/example"
+  "/Users/matteodelseppia/Desktop/cpp_cmake_catch2_project_template/bin/example.pdb"
+  "CMakeFiles/example.dir/example.cpp.o"
+  "CMakeFiles/example.dir/example.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
